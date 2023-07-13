2db0be827a00f5fc27db70d77d8fac0de4a0711f
saripagurek <saripvm@gmail.com>
2023-07-13 14:35:50 -0400

    filter by participant ID

--- Switch-Point Analysis/main.py
+++ Switch-Point Analysis/main.py
@@ -11,7 +11,9 @@ signal, bkps = rpt.pw_constant(n_samples, dim, n_bkps, noise_std=sigma)
 '''
 
 #data = pd.read_csv('data.csv', usecols=['Trial', 'Fix_X', 'Fix_Y', 'SceneFix'], nrows = 500)
-data = pd.read_csv('data_exploit.csv', usecols=['Trial', 'Fix_X', 'Fix_Y', 'SceneFix'])
+data = pd.read_csv('data_exploit.csv', usecols=['Trial', 'Fix_X', 'Fix_Y', 'SceneFix', 'ParticipantID'])
+data = data[data['ParticipantID'] == 9]
+data = data.drop(columns=['ParticipantID'])
 data.set_index("Trial", inplace=True)
 data = data.sort_index()
 
@@ -25,4 +27,6 @@ result = algo.predict(pen=1)
 
 rpt_rewrite.display(test, result, result)
 plt.xlabel("Fixation #")
+plt.suptitle("PID: 9", fontsize=24)
+plt.tight_layout()
 plt.show()
