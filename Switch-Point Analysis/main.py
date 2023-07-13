39aea552ac543ecad672f41265d9e6a1090434b6
saripagurek <saripvm@gmail.com>
2023-07-13 14:04:43 -0400

    run with data

--- Switch-Point Analysis/main.py
+++ Switch-Point Analysis/main.py
@@ -1,15 +1,26 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import ruptures as rpt
 
+'''
 # generate signal
 n_samples, dim, sigma = 1000, 3, 4
 n_bkps = 4  # number of breakpoints
 signal, bkps = rpt.pw_constant(n_samples, dim, n_bkps, noise_std=sigma)
+'''
 
-# detection
-algo = rpt.Pelt(model="rbf").fit(signal)
-result = algo.predict(pen=10)
+data = pd.read_csv('data.csv', usecols=['Trial', 'Fix_X', 'Fix_Y', 'SceneFix'], nrows = 500)
+data.set_index("Trial", inplace=True)
+data = data.sort_index()
 
-# display
-rpt.display(signal, bkps, result)
+print(data)
+
+test = data.values.reshape(-1, 3)
+
+algo = rpt.Pelt(model="l2", min_size=28)
+algo.fit(test)
+result = algo.predict(pen=1)
+
+rpt.display(test, result, result)
+plt.xlabel("Fixation #")
 plt.show()
